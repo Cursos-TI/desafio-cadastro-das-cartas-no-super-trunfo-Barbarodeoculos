f0445158ded621b7d4c817c3289d2ffd8ced051f
Barbarodeoculos <alex.carvalhoxd@hotmail.com>
2025-02-01 01:41:11 +0000

    melhorei a visualizacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -22,47 +22,52 @@ int main() {
     char estado[20];
     char cidade[20];
     char cod[20];
-    int populacao;
-    int turistico;
-    float area;
-    double pib;
-
-
+         int populacao;
+         int turistico;
+             float area;
+                   double pib;
 
     printf("insira o país:\n");
     scanf(" %s", &pais);
 
-    printf("Insira o Estado:\n");
-    scanf(" %s", &estado);
+          printf("Insira o Estado:\n");
+          scanf(" %s", &estado);
 
-    printf("Insira a cidade:\n");
-    scanf(" %s", &cidade);
+                printf("Insira a cidade:\n");
+                scanf(" %s", &cidade);
 
-    printf("Código da carta:\n");
-    scanf(" %s", &cod);
+                      printf("Código da carta:\n");
+                      scanf(" %s", &cod);
          
     printf("Insira a população:\n");
     scanf(" %d", &populacao);
 
-    printf("Insira a Área da cidade:\n");
-    scanf(" %f", &area);
+          printf("Insira a Área da cidade:\n");
+          scanf(" %f", &area);
 
-    printf("Insira o PIB da cidade:\n");
-    scanf(" %e", &pib);
+                printf("Insira o PIB da cidade:\n");
+                scanf(" %e", &pib);
 
-    printf("Insira quantos pontos turísticos possui a cidade:\n");
-    scanf(" %d", &turistico);
-    
+                      printf("Insira quantos pontos turísticos possui a cidade:\n");
+                      scanf(" %d", &turistico);
+
 // corrigir area e pib
 
     printf("País: %s \n", pais);
-    printf("Estado: %s \n", estado);
-    printf("Cidade: %s \n", cidade);
-    printf("Código: %s \n", cod);
+
+          printf("Estado: %s \n", estado);
+
+                printf("Cidade: %s \n", cidade);
+
+                      printf("Código: %s \n", cod);
+
     printf("População: %d \n", populacao);
-    printf("Área: %f \n", area);
-    printf("PIB: %.2e \n", pib);
-    printf("Pontos Turísticos: %d \n", turistico);
+
+          printf("Área: %f \n", area);
+
+                printf("PIB: %e \n", pib);
+
+                      printf("Pontos Turísticos: %d \n", turistico);
 
     return 0;
 }
