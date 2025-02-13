22c9c2037127e05ea6eced4a620a706e2fa78355
Barbarodeoculos <alex.carvalhoxd@hotmail.com>
2025-02-13 01:46:32 +0000

    acrescentei novas variaveis.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,54 +18,79 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    char pais[25];
-    char estado[25];
-    char cidade[25];
-    char cod[4];
-         int populacao;
-         int turistico;
-             float area;
-             float pib;
 
-    printf("insira o país:\n");
-    scanf(" %[^\n]", &pais);
+    // Declaração de variaveis;
+    // "Char" para um unico caractere, acrescentando "[n°]" para caracteres em cadeia(string);
+    // "int" para números inteiros;
+    // "float" para números de ponto flutuante que envolvam casas decimais;
 
-          printf("Insira o Estado:\n");
-          scanf(" %[^\n]", &estado);
+      // importante não usar acentos e nem "ç" para variaveis;
+      char pais[25];
+      char estado[25];
+      char cidade[25];
+      char cod[4];
+      int populacao;
+      int turistico;
+      float area;
+      float pib;
 
-                printf("Insira a cidade:\n");
-                scanf(" %[^\n]", &cidade);
+            //Especificadores de formato;
+            // (%d) para caracteres inteiros;
+            // (%s) para caracteres em cadeia;
+            // (%f) para caracteres de ponto flutuante, acrescentando (.)+(nº), define casa decimais a serem exibidas;
+            // ha outros especificadores de formato, mas como não os usarei, pelo menos por enquanto, não os descreverei;
 
-                      printf("Código da carta:\n");
-                      scanf(" %s", &cod);
+                  //"printf" para imprimir;
+                  //"scanf" para ler a impressão;
+                  printf("insira o país:\n");
+                  scanf(" %[^\n]", &pais);
+                  //"[^\n]" Foi a solução que encontrei para resolver o problema de espaçamento da string;
+                  //"[^\n]" permite a cadeia string até que o usuario pule de linha; 
+
+                        printf("Insira o Estado:\n");
+                        scanf(" %[^\n]", &estado);
+                        //"&" antes da variavel, para possibilitar a interação com o usuario na informação específica;
+                              printf("Insira a cidade:\n");
+                              scanf(" %[^\n]", &cidade);
+
+                                    printf("Código da carta:\n");
+                                    scanf(" %s", &cod);
          
-    printf("Insira a população:\n");
-    scanf(" %d", &populacao);
+                              printf("Insira a população:\n");
+                              scanf(" %d", &populacao);
 
-          printf("Insira a Área da cidade em km²:\n");
-          scanf(" %f", &area);
+                        printf("Insira a Área da cidade em km²:\n");
+                        scanf(" %f", &area);
 
-                printf("Insira o PIB da cidade em milhões:\n");
-                scanf(" %f", &pib);
+                  printf("Insira o PIB da cidade em milhões:\n");
+                  scanf(" %f", &pib);
 
-                      printf("Insira quantos pontos turísticos possui a cidade:\n");
-                      scanf(" %d", &turistico);
+            printf("Insira quantos pontos turísticos possui a cidade:\n");
+            scanf(" %d", &turistico);
 
-    printf("País: %s \n", pais);
+      printf("País: %s \n", pais);
 
-          printf("Estado: %s \n", estado);
+      printf("Estado: %s \n", estado);
 
-                printf("Cidade: %s \n", cidade);
+            printf("Cidade: %s \n", cidade);
 
-                      printf("Código: %s \n", cod);
+                  printf("Código: %s \n", cod);
 
-    printf("População: %d \n", populacao);
+                        printf("População: %d \n", populacao);
 
-          printf("Área: %3.f km² \n", area);
+                              printf("Área: %3.f km² \n", area);
 
-                printf("PIB: %3.f milhões \n", pib);
+                                    printf("PIB: %3.f milhões \n", pib);
 
-                      printf("Pontos Turísticos: %d \n", turistico);
+                              printf("Pontos Turísticos: %d \n", turistico);
+                        
+                        //Importante inserir variaveis que envolvão calculo antes da impressão, para não causar erro;
+                        float densidade = populacao/area;
+                        float capita = pib/populacao;
+                        
+                  printf("Densidade Populacional: %.2f \n", densidade);
 
-    return 0;
+            printf("Pib per capita: %.2f \n", capita);
+
+      return 0;
 }
